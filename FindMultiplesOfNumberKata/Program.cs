adee033024a4338602e046a182488f1976bdeb92
ColinJr <colindetwillerjr@gmail.com>
2022-02-11 13:25:01 -0500

    finished and cleaned MultiplesOfNumberKata

--- FindMultiplesOfNumberKata/Program.cs
+++ FindMultiplesOfNumberKata/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FindMultiplesOfNumberKata
 {
@@ -6,7 +7,25 @@ namespace FindMultiplesOfNumberKata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(FindMultiples(5, 25).ToString());
+        }
+        public static List<int> FindMultiples(int integer, int limit)
+        {
+            //infantile error handling
+            List<int> multiples = new List<int>();
+            if (integer <= 0 | limit <= 0)
+            {
+                return new List<int>(0);
+            }
+            //logic
+            for (int i = integer; i <= limit; i = i + integer)
+            {
+                multiples.Add(i);
+            }
+
+            return multiples;
         }
     }
 }
+//Assert.AreEqual(new List<int> { 5, 10, 15, 20, 25 }, Kata.FindMultiples(5, 25));
